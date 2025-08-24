751afd93a2697d77d39cf9f13a7d08366e1aa6a4
Bao Vuong <baovg.a1.k2023@gmail.com>
2025-08-24 16:04:09 -0400

    fix: updated extract_entity_content to use correct entity location field name; added condition to make sure comment start byte and end byte exists

--- src/deicide/corpus.py
+++ src/deicide/corpus.py
@@ -7,10 +7,12 @@ def extract_entity_content(file_content: bytes, entity: Entity) -> str:
     """
     Extracts the content of the given entity from the file content.
     """
-    if USE_DOC_COMMENTS:
-        start_byte = min(entity.start, entity.cmt_start)
-        end_byte = max(entity.end, entity.cmt_end)
+    if USE_DOC_COMMENTS and (
+        entity.comment_start_byte is not None and entity.comment_end_byte is not None
+    ):
+        start_byte = min(entity.start_byte, entity.comment_start_byte)
+        end_byte = max(entity.end_byte, entity.comment_end_byte)
     else:
-        start_byte = entity.start
-        end_byte = entity.end
+        start_byte = entity.start_byte
+        end_byte = entity.end_byte
     return file_content[start_byte:end_byte].decode()
