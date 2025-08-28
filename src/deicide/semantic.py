d12fcca69e42021ee867bbd3f220489d09a559c8
Bao Vuong <baovg.a1.k2023@gmail.com>
2025-08-28 15:40:16 -0400

    feat: implemented LSISimilarity following SemanticSimilarity abstract class, which use LSI to calculate similarity

--- src/deicide/semantic.py
+++ src/deicide/semantic.py
@@ -1,8 +1,11 @@
 import abc
 
 from deicide.kielaclark import KielaClark
+from deicide.lsi import LSI
+from deicide.corpus import tokenize_corpus
 
 KIELA_CLARK_MIN_SIM = 0.25
+LSI_MIN_SIM = 0.25
 
 
 class SemanticSimilarity(abc.ABC):
@@ -27,5 +30,21 @@ class KielaClarkSimilarity(SemanticSimilarity):
         a_doc, b_doc = self._corpus[a_id], self._corpus[b_id]
         score = self._kielaclark.sim(a_doc, b_doc)
         if score < KIELA_CLARK_MIN_SIM:
+            return 0.0
+        return score
+
+
+class LSISimilarity(SemanticSimilarity):
+    def __init__(self) -> None:
+        self._corpus: dict[str, str] = dict()
+
+    def fit(self, corpus: dict[str, str]) -> None:
+        self._corpus = corpus
+        self._lsi: LSI = LSI(tokenize_corpus(corpus))
+
+    def sim(self, a_id: str, b_id: str) -> float:
+        # a_doc, b_doc = self._corpus[a_id], self._corpus[b_id]
+        score: float = self._lsi.sim(a_id, b_id)
+        if score < LSI_MIN_SIM:
             return 0.0
         return score
